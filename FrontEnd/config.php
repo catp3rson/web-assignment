583ca4c2b7170935a5d12baacf184c218b085070
Khoa <khoavnpsc@gmail.com>
2021-11-17 19:00:39 +0700

    remove pass

--- FrontEnd/config.php
+++ FrontEnd/config.php
@@ -1,7 +1,7 @@
 <?php
 	$mysql_addr = "localhost";
 	$mysql_user = "root";
-	$mysql_password = "an0kumene";
+	$mysql_password = "";
 	$mysql_db = "tutor_booking_system";
 	$conn = new mysqli($mysql_addr, $mysql_user, $mysql_password);
 	$sql = "CREATE DATABASE IF NOT EXISTS tutor_booking_system;";
--- FrontEnd/register/tutor.php
+++ FrontEnd/register/tutor.php
@@ -2,7 +2,7 @@
     //connection details
     $mysql_addr = "localhost";
     $mysql_user = "root";
-    $mysql_password = "an0kumene";
+    $mysql_password = "";
     $mysql_db = "tutor_booking_system";
 
 
