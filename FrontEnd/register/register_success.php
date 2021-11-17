4ecf128f3ab16a1ce561210fbb015883497eb26b
catp3rson <ngan.nguyen1911667@hcmut.edu.vn>
2021-11-17 18:31:36 +0700

    Add registration dialog

---
+++ FrontEnd/register/register_success.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <link rel="stylesheet" type="text/css" href="register_success.css"/>
+    </head>
+
+    <body>
+        <!-- navigation bar -->
+        <nav id="top" class="navbar navbar-expand-lg fixed-top">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-supported-content" aria-controls="navbar-supported-content" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbar-supported-content">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../#courses">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacts</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div id="main-container" >
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-sm-10 col-10 offset-md-1 offset-sm-1 offset-1 p-0">
+                        <h1>THANK YOU FOR REGISTERING!<h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10 col-sm-10 col-10 offset-md-1 offset-sm-1 offset-1 p-0">
+                        <p>You will receive an email to confirm your registration</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
