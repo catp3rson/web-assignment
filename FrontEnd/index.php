bf55ee6c9c32dd17acc487dfbec79ff7ef6fc303
Nguyen Quoc Quy <quy.nguyen23062000@gmail.com>
2021-11-23 22:26:31 +0700

    title page

--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -17,7 +17,7 @@ else if(isset($_POST['page']))
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EDUBK</title>
+    <title>EDUBK - A tutor booking system</title>
     <link rel="icon" href="./assets/images/LOGO.png" type="image/x-icon" />
     <link rel="stylesheet" href="./assets/css/base.css">
     <link rel="stylesheet" href="./assets/css/main.css">
